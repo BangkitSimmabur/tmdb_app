f3c925579350a6ead18a21e4d14111d41c0a2454
Bangkit <bkt.simmabur@gmail.com>
2024-08-08 20:52:40 +0700

    create custom spinner

---
+++ lib/components/common_components/common_spinner.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:tmdb_app/helpers/constant.dart';
+
+
+class CommonSpinner extends StatelessWidget {
+  final double spinnerSize;
+  final Color? spinnerColor;
+
+  const CommonSpinner({super.key, this.spinnerSize = 50, this.spinnerColor});
+
+  @override
+  Widget build(BuildContext context) => Center(
+    child: SizedBox(
+      height: spinnerSize,
+      width: spinnerSize,
+      child:  SpinKitFadingCircle(
+        color: spinnerColor ?? Constant.vibrantLightBlue,
+      ),
+    ),
+  );
+}
