bfa86031230501f471d5a7eee1cfcc0eff06ef85
Bangkit <bkt.simmabur@gmail.com>
2024-08-08 20:52:53 +0700

    create custom text component

---
+++ lib/components/common_components/common_text.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:tmdb_app/helpers/constant.dart';
+
+class CommonText extends StatelessWidget {
+  final String text;
+  final double? fontSize;
+  final Color? textColor;
+  final FontWeight? fontWeight;
+  final TextAlign? textAlign;
+
+  const CommonText(
+      this.text, {
+        super.key,
+        this.fontSize = Constant.fontSizeSM,
+        this.textColor = Constant.black,
+        this.fontWeight,
+        this.textAlign,
+      });
+
+  @override
+  Widget build(BuildContext context) => Text(
+    text,
+    style: TextStyle(
+      fontSize: fontSize,
+      color: textColor,
+      fontWeight: fontWeight,
+    ),
+    textAlign: textAlign,
+  );
+}
