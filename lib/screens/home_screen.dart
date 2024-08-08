d66ef19c8a1fd2bff30d2c2a5a623ce1d79d678a
Bangkit <bkt.simmabur@gmail.com>
2024-08-08 20:54:37 +0700

    update component to use custom components add popular movies

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,12 @@
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:tmdb_app/components/common_components/common_spinner.dart';
+import 'package:tmdb_app/components/common_components/common_text.dart';
+import 'package:tmdb_app/components/movie_card.dart';
+import 'package:tmdb_app/helpers/constant.dart';
+import 'package:tmdb_app/models/movie.dart';
+import 'package:tmdb_app/services/movie_service.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -9,15 +16,34 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  late MovieService _movieService;
+  bool nowPlayingLoading = true;
+  bool popularLoading = true;
+  List<Movie> nowPlaying = [];
+  List<Movie> popularMovies = [];
+
+  @override
+  void initState() {
+    Future.delayed(Duration.zero, initDataState);
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
+    _movieService = Provider.of<MovieService>(context);
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.transparent,
+        backgroundColor: Constant.black,
         elevation: 0,
-        title: const Text("TMDB"),
+        title: const CommonText(
+          "HOME",
+          fontSize: Constant.fontSizeXL,
+          textColor: Constant.white,
+          fontWeight: FontWeight.bold,
+        ),
         centerTitle: true,
       ),
+      backgroundColor: Constant.black,
       body: SingleChildScrollView(
         physics: const BouncingScrollPhysics(),
         child: Padding(
@@ -25,44 +51,85 @@ class _HomeScreenState extends State<HomeScreen> {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              const Text(
+              const CommonText(
                 "Now Playing",
-                style: TextStyle(fontSize: 25),
+                fontSize: Constant.fontSizeLG,
+                fontWeight: FontWeight.bold,
+                textColor: Constant.white,
               ),
               Padding(
-                padding: const EdgeInsets.symmetric(vertical: 16.0),
+                padding:
+                    const EdgeInsets.symmetric(vertical: Constant.paddingMD),
                 child: SizedBox(
                   width: double.infinity,
-                  child: CarouselSlider.builder(
-                    itemCount: 20,
-                    itemBuilder: (context, itemIndex, pageViewIndex) {
-                      return ClipRRect(
-                        borderRadius: BorderRadius.circular(12),
-                        child: Container(
-                          height: 300,
-                          width: 200,
-                          color: Colors.red,
-                          child: Text(
-                            itemIndex.toString(),
+                  height: 300,
+                  child: nowPlayingLoading
+                      ? const CommonSpinner()
+                      : CarouselSlider.builder(
+                          itemCount:
+                              nowPlaying.length < 6 ? nowPlaying.length : 6,
+                          itemBuilder: (context, index, pageViewIndex) {
+                            return MovieCard(
+                              movie: nowPlaying[index],
+                            );
+                          },
+                          options: CarouselOptions(
+                            height: 300,
+                            autoPlay: false,
+                            enlargeCenterPage: true,
+                            viewportFraction: 0.55,
+                            autoPlayCurve: Curves.fastLinearToSlowEaseIn,
+                            autoPlayAnimationDuration:
+                                const Duration(seconds: 1),
                           ),
                         ),
-                      );
-                    },
-                    options: CarouselOptions(
-                      height: 300,
-                      autoPlay: false,
-                      enlargeCenterPage: true,
-                      viewportFraction: 0.55,
-                      autoPlayCurve: Curves.fastLinearToSlowEaseIn,
-                      autoPlayAnimationDuration: const Duration(seconds: 1),
-                    ),
-                  ),
                 ),
               ),
+              const CommonText(
+                "Popular",
+                fontSize: Constant.fontSizeLG,
+                fontWeight: FontWeight.bold,
+                textColor: Constant.white,
+              ),
+              popularLoading
+                  ? const CommonSpinner()
+                  : Column(
+                      children: listPopularMovies,
+                    )
             ],
           ),
         ),
       ),
     );
+  }
+
+  Future initDataState() async {
+    var fetchNowPlaying = await _movieService.getNowPlaying();
+    setState(() {
+      nowPlayingLoading = false;
+      nowPlaying = fetchNowPlaying ?? [];
+    });
+    var fetchPopularMovies = await _movieService.getPopularMovies();
+    setState(() {
+      popularLoading = false;
+      popularMovies = fetchPopularMovies ?? [];
+    });
+    return;
+  }
+
+  List<Widget> get listPopularMovies {
+    List<Widget> listWidgets = [];
+    popularMovies.asMap().forEach((int key, Movie movie) {
+      listWidgets.add(
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: Constant.paddingMD),
+          child: MovieCard(
+            movie: movie,
+          ),
+        ),
+      );
+    });
+
+    return listWidgets;
   }
 }
