43a3da1a62cf11c1f15fe9bf5d5a4309337f2be5
Bangkit <bkt.simmabur@gmail.com>
2024-08-09 19:26:49 +0700

    change outline button to stateless widget tweak component

--- lib/reusable_components/common_components/outline_button.dart
+++ lib/reusable_components/common_components/outline_button.dart
@@ -1,17 +1,15 @@
 import "package:flutter/material.dart";
-import 'package:flutter/cupertino.dart';
-import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:tmdb_app/helpers/constant.dart';
+import 'package:tmdb_app/reusable_components/common_components/common_text.dart';
 
-class CustomOutlinedButton extends StatefulWidget {
-  final String labelText;
+class CustomOutlinedButton extends StatelessWidget {
+  final String? labelText;
   final Color? labelColor;
   final Color? borderColor;
   final double labelSize;
   final Color? buttonColor;
   final VoidCallback? onTap;
   final double borderRadius;
-  final bool isLoading;
   final double width;
   final double padding;
   final bool active;
@@ -19,16 +17,16 @@ class CustomOutlinedButton extends StatefulWidget {
   final double height;
   final FontWeight labelWeight;
   final TextOverflow textOverflow;
+  final IconData? icon;
 
-  const CustomOutlinedButton(
-    this.labelText, {
+  const CustomOutlinedButton({
     super.key,
+    this.labelText,
     this.buttonColor = Constant.colorWhite,
     this.labelColor = Constant.colorBlack,
     this.labelSize = 16,
     this.onTap,
     this.borderRadius = Constant.borderRadiusSM,
-    this.isLoading = false,
     this.width = double.maxFinite,
     this.padding = Constant.paddingXS,
     this.active = false,
@@ -37,52 +35,56 @@ class CustomOutlinedButton extends StatefulWidget {
     this.textOverflow = TextOverflow.ellipsis,
     this.height = 4,
     this.borderColor = Constant.colorBlack,
+    this.icon,
   });
 
   @override
-  State<StatefulWidget> createState() {
-    return _CustomOutlinedButtonState();
-  }
-}
-
-class _CustomOutlinedButtonState extends State<CustomOutlinedButton> {
-  @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      width: widget.width,
-      child: ElevatedButton(
-        key: widget.key,
-        onPressed: !widget.isLoading ? widget.onTap : null,
-        style: ElevatedButton.styleFrom(
-          backgroundColor: widget.active ? Colors.white : widget.buttonColor,
-          shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(widget.borderRadius),
-              side:
-                  BorderSide(color: widget.borderColor ?? Colors.transparent)),
-        ),
-        child: Container(
-          padding: EdgeInsets.only(
-            top: widget.padding,
-            bottom: widget.padding,
-          ),
-          child: widget.isLoading
-              ? SpinKitThreeBounce(
-                  color: widget.labelColor,
-                  size: widget.labelSize,
-                )
-              : Text(
-                  widget.labelText,
-                  overflow: widget.textOverflow,
-                  maxLines: widget.maxLines,
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontWeight: widget.labelWeight,
-                    fontSize: widget.labelSize,
-                    color: widget.labelColor,
-                  ),
-                ),
-        ),
+    ButtonStyle buttonStyle = ElevatedButton.styleFrom(
+      backgroundColor: active ? Colors.white : buttonColor,
+      shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(borderRadius),
+          side: BorderSide(color: borderColor ?? Colors.transparent)),
+    );
+
+    Widget childElement = Container(
+      padding: EdgeInsets.only(
+        top: padding,
+        bottom: padding,
       ),
+      child: icon != null && labelText == null
+          ? Icon(
+              icon,
+              color: labelColor,
+            )
+          : CommonText(
+              labelText ?? "",
+              overflow: textOverflow,
+              maxLines: maxLines,
+              textAlign: TextAlign.center,
+              fontWeight: labelWeight,
+              fontSize: labelSize,
+              textColor: labelColor,
+            ),
+    );
+
+    return SizedBox(
+      width: width,
+      child: icon != null && labelText != null
+          ? ElevatedButton.icon(
+              icon: Icon(
+                icon,
+                color: labelColor,
+              ),
+              onPressed: onTap,
+              style: buttonStyle,
+              label: childElement,
+            )
+          : ElevatedButton(
+              onPressed: onTap,
+              style: buttonStyle,
+              child: childElement,
+            ),
     );
   }
 }
