30aaeed45b81cfd63feb04c7863d531bc28e84b1
Bangkit <bkt.simmabur@gmail.com>
2024-08-09 19:29:05 +0700

    remove unused import

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,4 +1,3 @@
-import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:tmdb_app/helpers/constant.dart';
