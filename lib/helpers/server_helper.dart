363d3e32bcdab9c2e21501ffbe39437c1976a9a4
Bangkit <bkt.simmabur@gmail.com>
2024-08-10 21:37:14 +0700

    add doc comments

--- lib/helpers/server_helper.dart
+++ lib/helpers/server_helper.dart
@@ -1,7 +1,15 @@
+/// A class created to hale the api call responses
 class HandlingServerLog {
+  /// Variable containing api call status from tmdb
   int? status;
+
+  /// Variable containing the whole response from api calls
   dynamic data;
+
+  /// Variable containing response message from api calls
   String? message;
+
+  /// Variable containing the api call status either success or fail
   bool? success;
 
   HandlingServerLog(
