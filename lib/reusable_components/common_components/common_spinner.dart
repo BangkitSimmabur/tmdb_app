2013bc6d48bef49f38422573efbd95c78dd2fb80
Bangkit <bkt.simmabur@gmail.com>
2024-08-10 20:12:18 +0700

    add doc comments and reformating file

--- lib/reusable_components/common_components/common_spinner.dart
+++ lib/reusable_components/common_components/common_spinner.dart
@@ -3,21 +3,28 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:tmdb_app/helpers/constant.dart';
 
-
+/// A custom reusable spinner to display loading
 class CommonSpinner extends StatelessWidget {
+  /// Spinner size default to 50px
   final double spinnerSize;
+
+  /// Spinner color default to dark red
   final Color? spinnerColor;
 
-  const CommonSpinner({super.key, this.spinnerSize = 50, this.spinnerColor});
+  const CommonSpinner({
+    super.key,
+    this.spinnerSize = 50,
+    this.spinnerColor = Constant.colorRed,
+  });
 
   @override
   Widget build(BuildContext context) => Center(
-    child: SizedBox(
-      height: spinnerSize,
-      width: spinnerSize,
-      child:  SpinKitRing(
-        color: spinnerColor ?? Constant.colorRed,
-      ),
-    ),
-  );
+        child: SizedBox(
+          height: spinnerSize,
+          width: spinnerSize,
+          child: const SpinKitRing(
+            color: Constant.colorRed,
+          ),
+        ),
+      );
 }
