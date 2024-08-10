c1d75b1db0c349478bfe54d0120055d7130a2595
Bangkit <bkt.simmabur@gmail.com>
2024-08-10 21:37:03 +0700

    remove unused variable and add doc comments

--- lib/helpers/secure_storage_helper.dart
+++ lib/helpers/secure_storage_helper.dart
@@ -1,6 +1,8 @@
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
+/// Class to handle data on secured storage, data type store is only String
 class SecureStorageHelper {
+  /// Function to store data
   static Future<void> writeSecureStorage(
     String key,
     String value,
@@ -8,17 +10,17 @@ class SecureStorageHelper {
     await const FlutterSecureStorage().write(key: key, value: value);
   }
 
-  static Future<String?> getSecureStorage(String key) async {
-    return await const FlutterSecureStorage().read(key: key);
-  }
-
+  /// Function to delete stored data based on data's key
   static Future<void> deleteSecureStorage(String key) async {
     await const FlutterSecureStorage().delete(key: key);
   }
 
+  /// Function to specifically fetch session id
   static Future<String?> getSession() async {
     return await const FlutterSecureStorage().read(key: "sessionID");
   }
+
+  /// Function to specifically fetch user id
   static Future<String?> getUserID() async {
     return await const FlutterSecureStorage().read(key: "userID");
   }
