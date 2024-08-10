7618359b0dfab067430df197d146ea49fe61df78
Bangkit <bkt.simmabur@gmail.com>
2024-08-10 14:16:38 +0700

    Add favorites and watchlist in profile page

---
+++ lib/screens/profile/profile_movies_component.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tmdb_app/helpers/constant.dart';
+import 'package:tmdb_app/models/movie.dart';
+import 'package:tmdb_app/reusable_components/common_components/common_spinner.dart';
+import 'package:tmdb_app/reusable_components/movie_card.dart';
+
+class ProfileMovieComponent extends StatelessWidget {
+  final List<Movie> movies;
+  final bool isLoading;
+  final ScrollController scrollController;
+  final Future<void> Function(int?) onRemoveFromList;
+
+  const ProfileMovieComponent({
+    super.key,
+    required this.isLoading,
+    required this.scrollController,
+    required this.movies,
+    required this.onRemoveFromList,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 400,
+      width: double.infinity,
+      child: isLoading
+          ? const CommonSpinner()
+          : ListView.builder(
+              controller: scrollController,
+              scrollDirection: Axis.horizontal,
+              key: UniqueKey(),
+              itemBuilder: (BuildContext context, int index) {
+                return Padding(
+                  padding: const EdgeInsets.all(
+                    Constant.paddingMD,
+                  ),
+                  child: SizedBox(
+                    height: 368,
+                    width: 245,
+                    child: Stack(
+                      children: [
+                        MovieCard(movies[index]),
+                        Align(
+                          alignment: Alignment.topRight,
+                          child: IconButton(
+                            style: IconButton.styleFrom(
+                              backgroundColor: Constant.colorWhiteOpacity75,
+                            ),
+                            icon: const Icon(
+                              FontAwesomeIcons.trash,
+                              color: Constant.colorBlackOpacity75,
+                            ),
+                            onPressed: () {
+                              onRemoveFromList!(movies[index].id);
+                            },
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              },
+              itemCount: movies.length,
+            ),
+    );
+  }
+}
--- lib/screens/profile/profile_screen.dart
+++ lib/screens/profile/profile_screen.dart
@@ -1,4 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:tmdb_app/helpers/constant.dart';
+import 'package:tmdb_app/helpers/platform_helper.dart';
+import 'package:tmdb_app/helpers/server_helper.dart';
+import 'package:tmdb_app/models/movie.dart';
+import 'package:tmdb_app/reusable_components/common_components/button_full.dart';
+import 'package:tmdb_app/reusable_components/common_components/common_app_bar.dart';
+import 'package:tmdb_app/reusable_components/common_components/common_text.dart';
+import 'package:tmdb_app/screens/profile/profile_movies_component.dart';
+import 'package:tmdb_app/services/auth_service.dart';
+import 'package:tmdb_app/services/constant_service.dart';
+import 'package:tmdb_app/services/movie_service.dart';
 
 class ProfileScreen extends StatefulWidget {
   const ProfileScreen({super.key});
@@ -8,8 +20,248 @@ class ProfileScreen extends StatefulWidget {
 }
 
 class _ProfileScreenState extends State<ProfileScreen> {
+  late MovieService _movieService;
+  late ConstantService _constantService;
+  late AuthService _authService;
+  final favoriteScrollController = ScrollController();
+  final watchListScrollController = ScrollController();
+  bool favoriteLoading = true;
+  bool watchListLoading = true;
+  List<Movie> favoriteMovies = [];
+  List<Movie> watchListMovies = [];
+  int favoritePage = 1;
+  int watchListPage = 1;
+  int? favoriteMaxPage;
+  int? watchListMaxPage;
+
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(Duration.zero, initDataState);
+    favoriteScrollController.addListener(() {
+      if (favoriteScrollController.position.maxScrollExtent ==
+          favoriteScrollController.offset) {
+        fetchFavoriteMovie();
+      }
+    });
+    watchListScrollController.addListener(() {
+      if (watchListScrollController.position.maxScrollExtent ==
+          watchListScrollController.offset) {
+        fetchFavoriteMovie();
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    favoriteScrollController.dispose();
+    watchListScrollController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    _movieService = Provider.of<MovieService>(context);
+    _constantService = Provider.of<ConstantService>(context);
+    _authService = Provider.of<AuthService>(context);
+    print(_constantService.sessionID);
+
+    return Scaffold(
+      backgroundColor: Constant.colorBlack,
+      appBar: const CommonAppBar(
+        "Profile",
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  const Padding(
+                    padding: EdgeInsets.symmetric(vertical: Constant.paddingMD),
+                    child: CommonText(
+                      "Favorite Movies",
+                      textColor: Constant.colorWhite,
+                      fontSize: Constant.fontSizeLG,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  ProfileMovieComponent(
+                    isLoading: favoriteLoading,
+                    scrollController: favoriteScrollController,
+                    movies: favoriteMovies,
+                      onRemoveFromList: onRemoveFavoriteMovie,
+                      // onRemoveFromList: (123){},
+                  ),
+                  const Padding(
+                    padding:
+                        EdgeInsets.symmetric(vertical: Constant.fontSizeMD),
+                    child: Divider(
+                      color: Constant.colorDarkRed,
+                    ),
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.symmetric(vertical: Constant.paddingMD),
+                    child: CommonText(
+                      "Watch List",
+                      textColor: Constant.colorWhite,
+                      fontSize: Constant.fontSizeLG,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  ProfileMovieComponent(
+                    isLoading: watchListLoading,
+                    scrollController: watchListScrollController,
+                    movies: watchListMovies,
+                    onRemoveFromList: onRemoveWatchList,
+                  ),
+                ],
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 40,
+            child: CustomButtonFullBordered(
+              'Log out',
+              labelColor: Colors.black,
+              labelSize: 16,
+              labelWeight: FontWeight.w600,
+              buttonColor: Constant.colorWhite,
+              borderRadius: 4,
+              onClick: () {},
+              btnBorderSide:
+                  const BorderSide(width: 4, color: Constant.colorWhite),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Future initDataState() async {
+    fetchFavoriteMovie();
+    fetchWatchList();
+  }
+
+  Future<void> fetchFavoriteMovie() async {
+    if (favoriteMaxPage == favoritePage) {
+      return;
+    }
+
+    favoriteLoading == false
+        ? setState(() {
+            favoriteLoading = true;
+          })
+        : {};
+    var serverLog = await _movieService.getFavoriteMovies(favoritePage);
+
+    if (serverLog.data["results"] != null) {
+      var movies = moviesFromJson(serverLog.data['results']);
+      setState(() {
+        favoriteMovies = [...favoriteMovies, ...movies ?? []];
+        favoriteMaxPage = serverLog.data["total_pages"];
+      });
+      if (favoritePage < favoriteMaxPage!) {
+        setState(() {
+          favoritePage = favoritePage++;
+        });
+      }
+    }
+
+    setState(() {
+      favoriteLoading = false;
+    });
+    return;
+  }
+
+  Future<void> fetchWatchList() async {
+    if (watchListMaxPage == watchListPage) {
+      return;
+    }
+
+    watchListLoading == false
+        ? setState(() {
+            watchListLoading = true;
+          })
+        : {};
+    var serverLog = await _movieService.getWatchList(watchListPage);
+
+    if (serverLog.data["results"] != null) {
+      var movies = moviesFromJson(serverLog.data['results']);
+      setState(() {
+        watchListMovies = [...watchListMovies, ...movies ?? []];
+        watchListMaxPage = serverLog.data["total_pages"];
+      });
+      if (watchListPage < watchListMaxPage!) {
+        setState(() {
+          watchListPage = watchListPage++;
+        });
+      }
+    }
+
+    setState(() {
+      watchListLoading = false;
+    });
+    return;
+  }
+
+  Future<void> onRemoveFavoriteMovie(int? id) async {
+    PlatformHelper.showLoadingAlert(context, 'Removing movie from favorites');
+
+    HandlingServerLog result = await _movieService.addToFavorites(id!, false);
+
+    mounted ? PlatformHelper.backTransitionPage(context) : {};
+
+    if (result.success == false) {
+      mounted
+          ? PlatformHelper.showErrorSnackbar(
+          context, "Failed to remove movie from favorites")
+          : {};
+      return;
+    }
+
+    if (result.success == true ) {
+      mounted
+          ? PlatformHelper.showSuccessSnackbar(
+          context, "Movie removed from favorites")
+          : {};
+      setState(() {
+        favoriteMovies.removeWhere((e) => e.id == id);
+      });
+      return;
+    }
+
+    return;
+  }
+
+  Future<void> onRemoveWatchList(int? id) async {
+    PlatformHelper.showLoadingAlert(context, 'Removing movie from watch list');
+
+    HandlingServerLog result = await _movieService.onAddToWatchList(id!, false);
+
+    mounted ? PlatformHelper.backTransitionPage(context) : {};
+
+    if (result.success == false) {
+      mounted
+          ? PlatformHelper.showErrorSnackbar(
+          context, "Failed to remove movie from your watch list")
+          : {};
+      return;
+    }
+
+
+    if (result.success == true ) {
+      mounted
+          ? PlatformHelper.showSuccessSnackbar(
+          context, "Movie removed from your watch list")
+          : {};
+
+      setState(() {
+        watchListMovies.removeWhere((e) => e.id == id);
+      });
+      return;
+    }
+
+    return;
   }
 }
